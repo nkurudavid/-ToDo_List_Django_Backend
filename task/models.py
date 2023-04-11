9840f553b09503a9ddba2cd6927f7c7d7f4ad02d
nkurudavid <nkurudavid44@gmail.com>
2023-04-11 19:34:48 +0200

    created task model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    TODO = 'todo'
+    DONE = 'dome'
+
+    STATUS_CHOICE = (
+        (TODO, 'ToDo'),
+        (DONE, 'Done'),
+    )
+    title = models.CharField(verbose_name="Task Title", max_length=50, null=False)
+    description = models.TextField(verbose_name="Task Description", null=False)
+    status = models.CharField(verbose_name="Status", max_length=10, choices=STATUS_CHOICE, default=TODO)
+    created_date = models.DateField(verbose_name="When Created", auto_now_add=True)
+    modified_date = models.DateField(verbose_name="When Modified", auto_now=True)
+
+    def __str__(self):
+        return self.title
