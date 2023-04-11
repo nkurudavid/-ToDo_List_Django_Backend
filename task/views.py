85ad6512c06a257c27da339d49d5542fa9d89d89
nkurudavid <nkurudavid44@gmail.com>
2023-04-11 20:00:14 +0200

    created task view

--- task/views.py
+++ task/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Task
+from .serializers import TaskSerializer
+
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    authentication_classes = (BaseAuthentication,)
+    permission_classes = (IsAuthenticated,)
